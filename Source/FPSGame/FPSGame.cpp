af81fee7f0a7b8f4f0b2a15fdd32318cd5c6bc22
williamwsun <sunwayliving@gmail.com>
2021-03-12 10:04:00 +0800

    1. copy from former proj

---
+++ Source/FPSGame/FPSGame.cpp
@@ -0,0 +1,8 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+#include "FPSGame.h"
+#include "Modules/ModuleManager.h"
+
+//一个游戏只能有一个primary game module
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FPSGame, "FPSGame" );
+ 
